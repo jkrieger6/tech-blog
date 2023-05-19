d9ba3c998356f4e6ae8922c30b97066f468e17ff
jkrieger6 <jkrieger11@gmail.com>
2023-05-19 08:23:31 -0700

    created database

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+-- Delete the database if it exists already --
+DROP DATABASE IF EXISTS `tech_blogDB`;
+-- Create the database --
+CREATE DATABASE `tech_blogDB`;
+-- Use the database --
+USE `tech_blogDB`;
