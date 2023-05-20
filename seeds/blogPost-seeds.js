aa12504a2d447f18417016dfc7c95e62c99d7ef4
jkrieger6 <jkrieger11@gmail.com>
2023-05-20 13:11:50 -0700

    added seeds to blog post for testing

---
+++ seeds/blogPost-seeds.js
@@ -0,0 +1,28 @@
+const { BlogPost } = require('../models');
+
+const blogPostData = [
+    {
+        title: 'Blog Post 1',
+        content: 'This is the first blog post.',
+        user_id: 1
+    },
+    {
+        title: 'Blog Post 2',
+        content: 'This is the second blog post.',
+        user_id: 2
+    },
+    {
+        title: 'Blog Post 3',
+        content: 'This is the third blog post.',
+        user_id: 3
+    },
+    {
+        title: 'Blog Post 4',
+        content: 'This is the fourth blog post.',
+        user_id: 4
+    },
+];
+
+const seedBlogPosts = () => BlogPost.bulkCreate(blogPostData);
+
+module.exports = seedBlogPosts;
