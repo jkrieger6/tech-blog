b001fd41169b1ef0b18a910e72cec3cbb9a2b828
jkrieger6 <jkrieger11@gmail.com>
2023-05-20 13:12:14 -0700

    added comment seeds for testing

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,28 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+        comment_text: 'Comment 1',
+        user_id: 1,
+        blogPost_id: 1
+    },
+    {
+        comment_text: 'Comment 2',
+        user_id: 2,
+        blogPost_id: 2
+    },
+    {
+        comment_text: 'Comment 3',
+        user_id: 3,
+        blogPost_id: 3
+    },
+    {
+        comment_text: 'Comment 4',
+        user_id: 4,
+        blogPost_id: 4
+    },
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
