9b53fd0c263942f9f80f4ccb2cdda4996247b2bd
jkrieger6 <jkrieger11@gmail.com>
2023-05-20 12:49:38 -0700

    updated a router.use connection

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', dashboardRoutes);
 
 module.exports = router;
