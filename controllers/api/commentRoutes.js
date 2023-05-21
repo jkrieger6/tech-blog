3ca2757969014f9618483318e1700b9a95b7206d
jkrieger6 <jkrieger11@gmail.com>
2023-05-21 16:52:31 -0700

    moved a file out of a folder, added routes to get post and comments to files

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,26 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// GET all comments
+router.get('/', async (req, res) => {
+    try {
+      const commentData = await Comment.findAll();
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// GET one comment
+router.get('/:id', async (req, res) => {
+    try {
+      const commentData = await Comment.findByPk(req.params.id);
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 // CREATE new comment
 router.post('/', withAuth, async (req, res) => {
     try {
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,15 @@
 // API routes for the application
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
-const commentRoutes = require('./commentRoutes');
-const homeRoutes = require('./home-routes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const blogRoutes = require("./blogRoutes");
+const commentRoutes = require("./commentRoutes");
+const homeRoutes = require("../home-routes");
 
 // Prefix all of the above routes with /api
-router.use('/users', userRoutes);
-router.use('/posts', blogRoutes);
-router.use('/comments', commentRoutes);
-router.use('/', homeRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", blogRoutes);
+router.use("/comments", commentRoutes);
+router.use("/", homeRoutes);
 
 // Exporting the router
 module.exports = router;
