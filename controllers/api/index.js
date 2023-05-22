af3f69a08588780486d67b677795730988b0e867
jkrieger6 <jkrieger11@gmail.com>
2023-05-21 17:06:51 -0700

    changed routes to mirror actual file path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,12 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const blogRoutes = require("./blogRoutes");
 const commentRoutes = require("./commentRoutes");
-const homeRoutes = require("../home-routes");
+const withAuth = require("../../utils/auth");
 
 // Prefix all of the above routes with /api
 router.use("/users", userRoutes);
 router.use("/posts", blogRoutes);
 router.use("/comments", commentRoutes);
-router.use("/", homeRoutes);
 
 // Exporting the router
 module.exports = router;
