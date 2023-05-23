79d4c82fe31950b1c3b0c03c137abbe913b1add6
jkrieger6 <jkrieger11@gmail.com>
2023-05-23 15:17:41 -0700

    removed route to GET one post, its not needed in homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User, Comment, BlogPost } = require("../models");
 
-// GET all posts for homepage
+// GET all posts for homepage and the assoicaited comments
 router.get("/", async (req, res) => {
   try {
     const postData = await BlogPost.findAll({
@@ -21,37 +21,6 @@ router.get("/", async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render("homepage", {
       posts,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// GET one post
-router.get("/:id", async (req, res) => {
-  try {
-    const postData = await BlogPost.findByPk(req.params.id, {
-      include: [
-        {
-          model: BlogPost,
-          attributes: ["title", "content", "created_at"],
-          include: [
-            {
-              model: User,
-              attributes: ["name"],
-            },
-            {
-              model: Comment,
-              attributes: ["comment_text"],
-            },
-          ],
-        },
-      ],
-    });
-    const post = postData.get({ plain: true });
-    res.render("BlogPost", {
-      post: post,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
