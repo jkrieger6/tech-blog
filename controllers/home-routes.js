7f2e14ee710cbc0af2b4dfbe9fd0b1e7c35af19c
jkrieger6 <jkrieger11@gmail.com>
2023-05-24 08:22:35 -0700

    fixed login route typo that was causing errors

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res) => {
 });
 
 // Login route
-router.render("/login", (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");
     return;
