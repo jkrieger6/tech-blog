5fa6dc7ff4fd77c425ed278386153ea2408348ae
jkrieger6 <jkrieger11@gmail.com>
2023-05-25 15:31:42 -0700

    added event handler to view blog post on view post button, added that file to the main handlebars script

---
+++ public/js/viewBlogPost.js
@@ -0,0 +1,22 @@
+// Handler to view a blog post
+const viewPostHandler = async (event) => {
+    event.preventDefault();
+
+    // Get the post id from the url
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    // GET the post from the database
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    // If successful, redirect the browser to the dashboard page
+    if (response.ok) {
+        document.location.replace(`/posts/${post_id}`);
+    }
+}
+
+document.getElementById('view-post').addEventListener('click', viewPostHandler);
