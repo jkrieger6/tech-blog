35e4e28caf62c60637d522cee644005431d7d56f
jkrieger6 <jkrieger11@gmail.com>
2023-05-25 18:44:28 -0700

    fixed helper function to get actual date, removed comments from homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User, Comment, BlogPost } = require("../models");
 
-// GET all posts for homepage and the assoicaited comments
+// GET all posts for homepage
 router.get("/", async (req, res) => {
   try {
     const postData = await BlogPost.findAll({
@@ -11,17 +11,12 @@ router.get("/", async (req, res) => {
             model: User,
             attributes: ["name"],
             },
-            {
-              model: Comment,
-              attributes: ["comment_text"],
-            },
           ],
         },
     );
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render("homepage", {
       posts,
-      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
